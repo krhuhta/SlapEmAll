7f7de82a44832d90b8e76096613a190ab187b3bc
NMyllari <nicholas.myllari@gmail.com>
2017-01-16 20:43:12 +0200

    Update Logic.scala

--- Logic.scala
+++ Logic.scala
@@ -5,10 +5,6 @@ class World {
   val directions = Vector(0, 1, 2, 3)                                          //North, East, South, West)
   val lineLength = 4
   val BoardSize = lineLength * 2 + 1
-//  private val lineOfEnemies: Array[Option[Enemy]] = Array.fill(lineLength + 1)(None)//single line of enemies. Index 0 is on top of player (player dies is here), 1 is next to player and lineLenght is the furtherst away. 
-//  val enemies: Array[Array[Option[Enemy]]] = Array.fill(4)(lineOfEnemies)  //4 lines of enemies. Index 0 is north, 1 is east, 2 is south and 3 is west
-  
-  //def tableOfEnemies = enemies.map(_.map(_.))
   var enemies: Array[Array[Option[Enemy]]] = Array.fill(BoardSize, BoardSize)(None)
   
   private var gameOver = false
@@ -25,18 +21,7 @@ class World {
     enemies = Array.fill(BoardSize, BoardSize)(None)
     unLose()
   }
-  
-//   def newEnemy() = {
-//     enemies(shuffle(directions).head)(lineLength) = Option(new Enemy)    //Creates a new enemy to a random line's last index
-//   }
-//   def advanceEnemies() = {                                                     //Moves all enemies closer the the player by 1 step.
-//       for (i <- 0 until 4 ; j <- 0 to lineOfEnemies.length) {
-//         if (enemies(i)(j).isDefined) {
-//           enemies(i)(j-1) = enemies(i)(j)
-//           enemies(i)(j) = None 
-//         }
-//       }
-//   }
+
   def newEnemy() = {
     val rand = shuffle(directions).head
     rand match {
@@ -76,20 +61,16 @@ class World {
   def advanceGame() = {                                                          //Advances the game
     advanceEnemies()
     newEnemy()
-    // if (!enemies.forall(_.head.isEmpty)) lose()
     if (enemies(lineLength)(lineLength).isDefined) lose() //Checks if an enemy is on top of the player (and loses the game if true)
     points += 10    
-    // ??? graphics
   }
   
   def advanceGameDoubles() = {                                                          //Advances the game
     advanceEnemies()
     newEnemy()
     if (time > 10 && time % 10 == 0) newEnemy()
-    // if (!enemies.forall(_.head.isEmpty)) lose()
     if (enemies(lineLength)(lineLength).isDefined) lose() //Checks if an enemy is on top of the player (and loses the game if true)
     points += 12
-    // ??? graphics
     time += 1
   }
 }
@@ -101,19 +82,26 @@ class Enemy {                  //Class for the Enemies, currently does nothing m
 }
   
 class Player(world: World) {                 //The player
-  
-//  def hit(dir: Int) = {                      //Determins if there is an enemy to kill in the direction of the hit. Kills it.
-//     if (world.enemies(dir)(1).isDefined) {   
-//       ??? //Graphics
-//       world.enemies(dir)(1) = None
-//     } else ??? //Graphics
-//   }
+  var dirOfLastHit = -1
+  var timeFromLastHit = 0
   def hit(dir: Int) = {                      // Hits in the direction of param dir. Kills whatever there is.
     dir match {
-      case 0 => world.enemies(world.lineLength)(world.lineLength - 1) = None // North
-      case 1 => world.enemies(world.lineLength + 1)(world.lineLength) = None // East
-      case 2 => world.enemies(world.lineLength)(world.lineLength + 1) = None // South
-      case 3 => world.enemies(world.lineLength - 1)(world.lineLength) = None // West
+      case 0 => {
+        world.enemies(world.lineLength)(world.lineLength - 1) = None // North
+        dirOfLastHit = dir
+      }
+      case 1 => {
+        world.enemies(world.lineLength + 1)(world.lineLength) = None // East
+        dirOfLastHit = dir
+      }
+      case 2 => {
+        world.enemies(world.lineLength)(world.lineLength + 1) = None // South
+        dirOfLastHit = dir
+      }
+      case 3 => {
+        world.enemies(world.lineLength - 1)(world.lineLength) = None // West
+        dirOfLastHit = dir
+      }
       case _ => None
     }
   }
